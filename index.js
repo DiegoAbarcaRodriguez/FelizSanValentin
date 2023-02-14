f011cbadb1c8909f2fa134619a765ded65e7fdff
DiegoAbarcaRodriguez <abarcarodriguezdiego@gmail.com>
2023-02-14 11:43:41 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btnYes = document.querySelector('#btn-yes');
+    const btnNo = document.querySelector('#btn-no');
+    let sound = new Audio('./audio/audio.mp3');
+
+    btnNo.addEventListener('mouseover', () => {
+        btnNo.style.position = 'absolute';
+        btnNo.style.top = (Math.random() * window.innerHeight) + 'px';
+        btnNo.style.left = (Math.random() * window.innerWidth) + 'px';
+    });
+
+    btnYes.addEventListener('click', () => {
+        sound.play();
+
+        Swal.fire({
+            title: 'Sabía que dirías que sí 😍😍😍',
+            width: 600,
+            padding: '3em',
+            color: 'crimson',
+            backdrop: `
+              url("./img/nyan-cat.gif")
+              left top
+              no-repeat
+            `
+        })
+    });
+});
